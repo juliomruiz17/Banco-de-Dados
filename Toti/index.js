2de8e1857ba5be19cab391562a89c4a24450b746
Julio Montoya <67755118+juliomruiz17@users.noreply.github.com>
2020-09-17 17:36:06 -0300

    Add files via upload

--- Toti/index.js
+++ Toti/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { Sequelize, DataTypes } = require('sequelize')
+const task = require('./models/task')
 const Task = require('./models/task')
 
 const app = express()
@@ -10,38 +11,40 @@ const tasks = Task(sequelize, DataTypes)
 app.use(express.json())
 
 // List tasks
-app.get('/tasks', (req, res) => {
-  res.json({ action: 'Listing tasks' })
-})
+app.get('/tasks', async (req, res) => {
+  tasks = await  Task.findAll();
+  return res.json(tasks)
+  }),
 
 // Create task
-app.post('/tasks', (req, res) => {
-  const body = req.body
-
-  res.json(body)
-})
+app.post('/tasks', async(req, res) => {
+  const { description, done} = req.body;
+  tasks = await tasks.create({description, done});
+  return res.json(tasks)
+}),
 
 // Show task
-app.get('/tasks/:id', (req, res) => {
-  const taskId = req.params.id
-
-  res.send({ action: 'Showing task', taskId: taskId })
-})
+app.get('/tasks/:id', async (req, res) => {
+  const { description,done } = req.body;
+  tasks = await tasks.findByPk(req.params.id)
+  return res.json(tasks)
+}),
 
 // Update task
-app.put('/tasks/:id', (req, res) => {
-  const taskId = req.params.id
-
-  res.send({ action: 'Updating task', taskId: taskId })
-})
+app.put('/tasks/:id', async (req, res) => {
+  const { description,done } = req.body;
+  tasks = await tasks.update({description, done})
+  return res.json(tasks)
+}),
 
 // Delete task
-app.delete('/tasks/:id', (req, res) => {
-  const taskId = req.params.id
-  
-  res.send({ action: 'Deleting task', taskId: taskId })
-})
+app.delete('/tasks/:id', async (req, res) => {
+  const{description} = req.params;
+  const{done} = req.body;
+   task = await Task.destroy(req.bosy)
+  return res.json(tasks)
+}),
 
-app.listen(3000, () => {
-  console.log('Iniciando o ExpressJS na porta 3000')
+app.listen(3000, () =>  {
+    console.log('Iniciando o Express JS na porta 3000')
 })
